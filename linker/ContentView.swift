751b2231b9b17a3bb2c88228433157918a6779f9
AlvaroSanz <sanzrodirgoalvaro@gmail.com>
2023-02-16 19:28:52 +0100

    Initial Commit

---
+++ linker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  linker
+//
+//  Created by Sanz Copacabana on 16/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ linker/linkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkerApp.swift
+//  linker
+//
+//  Created by Sanz Copacabana on 16/2/23.
+//
+
+import SwiftUI
+
+@main
+struct linkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
